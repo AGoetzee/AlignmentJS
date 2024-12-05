605acf1c21c3d2be7825a4768dab0d6aedb42cd6
Arthur <55135668+AGoetzee@users.noreply.github.com>
2024-12-05 18:15:32 +0100

    Formatting

--- alignment.js
+++ alignment.js
@@ -8,39 +8,39 @@ const MATCH_SCORE = 2;
 
 function constructMatrix(seq1, seq2) {
     let matrix = [];
-    for (let i = 0; i<seq1.length;i++) {
+    for (let i = 0; i < seq1.length; i++) {
         matrix.push([]); // row
-        for (let j = 0; j<seq2.length;j++){
+        for (let j = 0; j < seq2.length; j++) {
             matrix[i].push(0); // columns
         }
     }
 
-    return matrix
+    return matrix;
 }
 
 function initializeScoreMatrix(matrix, seq1, seq2) {
-    for (let i = 1; i<seq1.length; i++) {
-        matrix[i][0] = matrix[i-1][0] + GAP_PENALTY;
+    for (let i = 1; i < seq1.length; i++) {
+        matrix[i][0] = matrix[i - 1][0] + GAP_PENALTY;
     }
-    
+
     for (let j = 1; j < seq2.length; j++) {
-        matrix[0][j] = matrix[0][j-1] + GAP_PENALTY;
+        matrix[0][j] = matrix[0][j - 1] + GAP_PENALTY;
     }
-    
-    return matrix
+
+    return matrix;
 }
 
-function initializeTracebackMatrix (matrix, seq1, seq2) {
-    for (let i = 1; i<seq1.length; i++) {
-        matrix[i][0] = 'U';
+function initializeTracebackMatrix(matrix, seq1, seq2) {
+    for (let i = 1; i < seq1.length; i++) {
+        matrix[i][0] = "U";
     }
-    
+
     for (let j = 1; j < seq2.length; j++) {
-        matrix[0][j] = 'L';
+        matrix[0][j] = "L";
     }
-    matrix[0][0] = 'D';
-    
-    return matrix
+    matrix[0][0] = "D";
+
+    return matrix;
 }
 
 function isMatch(aa1, aa2) {
@@ -52,89 +52,90 @@ function isMatch(aa1, aa2) {
 }
 
 function calculateScores(scoreMatrix, tracebackMatrix, seq1, seq2) {
-    for (let i = 1; i<seq1.length; i++) {
-        for (let j = 1; j<seq2.length; j++) {
+    for (let i = 1; i < seq1.length; i++) {
+        for (let j = 1; j < seq2.length; j++) {
             const choices = {
-                'U': scoreMatrix[i-1][j] + GAP_PENALTY,
-                'L': scoreMatrix[i][j-1] + GAP_PENALTY,
-                'D': scoreMatrix[i-1][j-1] + isMatch(seq1[i],seq2[j])
-            }
+                U: scoreMatrix[i - 1][j] + GAP_PENALTY,
+                L: scoreMatrix[i][j - 1] + GAP_PENALTY,
+                D: scoreMatrix[i - 1][j - 1] + isMatch(seq1[i], seq2[j]),
+            };
             scoreMatrix[i][j] = Math.max(...Object.values(choices));
-            tracebackMatrix[i][j] = Object.entries(choices).reduce((max, current) => current[1] > max[1] ? current : max)[0];
+            tracebackMatrix[i][j] = Object.entries(choices).reduce(
+                (max, current) => (current[1] > max[1] ? current : max)
+            )[0];
         }
     }
-    return scoreMatrix, tracebackMatrix
+    return scoreMatrix, tracebackMatrix;
 }
 
 function traceback(tracebackMatrix, seq1, seq2) {
-    let alignment = ''
-    let alignmentComplement = ''
+    let alignment = "";
+    let alignmentComplement = "";
 
     let i = seq1.length - 1;
     let j = seq2.length - 1;
 
     while (i >= 0 && j >= 0) {
-            switch (tracebackMatrix[i][j]) {
-                case 'D':
-                    alignment = `${seq1[i]}${alignment}`; //seq1
-                    alignmentComplement = `${seq2[j]}${alignmentComplement}`; //seq2
-                    i--;
-                    j--;
-                    break
-                case 'U': //go a row Up
-                    alignment = `${seq1[i]}${alignment}`;
-                    alignmentComplement = `-${alignmentComplement}`;
-                    i--;
-                    break;
-                case 'L': //go a column Left
-                    alignment = `-${alignment}`;
-                    alignmentComplement = `${seq2[j]}${alignmentComplement}`;
-                    j--;
-                    break;
-            }
-
-
+        switch (tracebackMatrix[i][j]) {
+            case "D":
+                alignment = `${seq1[i]}${alignment}`; //seq1
+                alignmentComplement = `${seq2[j]}${alignmentComplement}`; //seq2
+                i--;
+                j--;
+                break;
+            case "U": //go a row Up
+                alignment = `${seq1[i]}${alignment}`;
+                alignmentComplement = `-${alignmentComplement}`;
+                i--;
+                break;
+            case "L": //go a column Left
+                alignment = `-${alignment}`;
+                alignmentComplement = `${seq2[j]}${alignmentComplement}`;
+                j--;
+                break;
+        }
     }
-    return {alignment, alignmentComplement};
-
+    return { alignment, alignmentComplement };
 }
 
 function prettyPrintMatrix(matrix, seq1, seq2) {
-    const header = ' ';
+    const header = " ";
     for (let char of seq2) {
         header += `   ${char}`;
     }
-    console.log(header)
+    console.log(header);
 
-    for (let i = 0; i < seq1.length; i++){
+    for (let i = 0; i < seq1.length; i++) {
         let row = `${seq1[i]}  `;
-        for (let j = 0 ;j < seq2.length; j++) {
-            row += `${matrix[i][j]}`.padEnd(4, ' ');
+        for (let j = 0; j < seq2.length; j++) {
+            row += `${matrix[i][j]}`.padEnd(4, " ");
         }
         console.log(row);
     }
 }
 
 function prettyPrintAlignment(alignment, alignmentComplement) {
-    const alignedMatch = (aa1, aa2) => aa1 === aa2 ? '|' : ' ';
-    
-    let topAlignmentRow = '';
-    let matchAlignmentRow = '';
-    let bottomAlignmentRow = '';
-    
-    for (let i = 0; i < alignment.length; i++){
-        
+    const alignedMatch = (aa1, aa2) => (aa1 === aa2 ? "|" : " ");
+
+    let topAlignmentRow = "";
+    let matchAlignmentRow = "";
+    let bottomAlignmentRow = "";
+
+    for (let i = 0; i < alignment.length; i++) {
         topAlignmentRow += `${alignment[i]}  `;
-        matchAlignmentRow += `${alignedMatch(alignment[i],alignmentComplement[i])}  `;
+        matchAlignmentRow += `${alignedMatch(
+            alignment[i],
+            alignmentComplement[i]
+        )}  `;
         bottomAlignmentRow += `${alignmentComplement[i]}  `;
     }
 
-    let result =`
+    let result = `
     ${topAlignmentRow}
     ${matchAlignmentRow}
-    ${bottomAlignmentRow}`
-    
-    return result
+    ${bottomAlignmentRow}`;
+
+    return result;
 }
 
 function printResults(alignment, alignmentComplement, scoreMatrix) {
@@ -153,46 +154,47 @@ function printResults(alignment, alignmentComplement, scoreMatrix) {
     Length: ${seq2.length}
 
     ------Results------
-    Alignment score: ${scoreMatrix[seq1.length-1][seq2.length-1]}
-    ${prettyPrintAlignment(alignment,alignmentComplement)}`;
-    
-    return result
+    Alignment score: ${scoreMatrix[seq1.length - 1][seq2.length - 1]}
+    ${prettyPrintAlignment(alignment, alignmentComplement)}`;
 
+    return result;
 }
 
 function validateSequences(seq1, seq2) {
-    if (seq1.length === 0 || seq2.length === 0){
-        throw new Error('Sequences cannot be empty!');
+    if (seq1.length === 0 || seq2.length === 0) {
+        throw new Error("Sequences cannot be empty!");
     }
 
-    if (typeof seq1 != 'string'|| typeof seq2 != 'string') {
-        throw new Error('Sequences must be strings!');
+    if (typeof seq1 != "string" || typeof seq2 != "string") {
+        throw new Error("Sequences must be strings!");
     }
 }
 
 export function runAlignment(seq1, seq2) {
-
     // step 1, initialization
     let scoreMatrix = constructMatrix(seq1, seq2);
     let tracebackMatrix = constructMatrix(seq1, seq2);
 
     scoreMatrix = initializeScoreMatrix(scoreMatrix, seq1, seq2);
-    tracebackMatrix = initializeTracebackMatrix(tracebackMatrix, seq1, seq2)
-    // step 2, calculation
-    [scoreMatrix, tracebackMatrix] = calculateScores(scoreMatrix, tracebackMatrix, seq1, seq2);
+    tracebackMatrix = initializeTracebackMatrix(tracebackMatrix, seq1, seq2)[
+        // step 2, calculation
+        (scoreMatrix, tracebackMatrix)
+    ] = calculateScores(scoreMatrix, tracebackMatrix, seq1, seq2);
 
     //step 3, traceback
-    const { alignment, alignmentComplement } = traceback(tracebackMatrix, seq1, seq2);
+    const { alignment, alignmentComplement } = traceback(
+        tracebackMatrix,
+        seq1,
+        seq2
+    );
 
     //step 4, print the results!
     return printResults(alignment, alignmentComplement, scoreMatrix);
-
-    
 }
 
 // Input variables
-let seq1 = 'AGCT'; //rows or i
-let seq2 = 'AGCT'; //columns or j
+let seq1 = "AGCT"; //rows or i
+let seq2 = "AGCT"; //columns or j
 validateSequences(seq1, seq2);
 
 console.log(runAlignment(seq1, seq2));
--- readme.md
+++ readme.md
@@ -1,19 +1,24 @@
 # AlignmentJS
+
 This is a javascript implementation of the [Needleman-Wunsch algorithm](https://en.wikipedia.org/wiki/Needleman–Wunsch_algorithm) for global sequence alignment. It is a simple implementation that I made to get a better understanding of javascript, as such you may encounter some beginner mistakes. The code is not optimized for performance, but it should work fine for small sequences. Feel free to give feedback or suggestions for improvements in the issues section.
 
 ## Usage
-Currently you need to hardcode the sequences and parameters in the script. The variables you need to change are:
-- `seq1` and `seq2`: the sequences to align
-- `GAP_PENALTY`, `MISMATCH_PENALTY` and `MATCH_SCORE`: the scoring parameters
 
-Then you can run the script in a browser, or with node.js: 
+Currently you need to hardcode the sequences and parameters in the script. The variables you need to change are:
+
+-   `seq1` and `seq2`: the sequences to align
+-   `GAP_PENALTY`, `MISMATCH_PENALTY` and `MATCH_SCORE`: the scoring parameters
+
+Then you can run the script in a browser, or with node.js:
+
 ```bash
 node alignment.js
-``` 
+```
 
 The results will be printed to the console.
 
 ## Example
+
 Currently, the script is set up to align the sequences "AGCT" and "AGCT". The output should be:
 
 ```bash
